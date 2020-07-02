92ea31f88e7f3e79d34b0130e6b6943c0963d2f8
YH <yh9407@gmail.com>
2020-07-02 14:41:41 +0900

    0702

--- Board/src/main/java/com/example/board/controller/BoardController.java
+++ Board/src/main/java/com/example/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.board.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,7 +55,65 @@ public class BoardController {
 		
 		return "1";
 	}
+	@GetMapping("/update/{id}")  // id 라는 명칭은 개발자가 임의로 만들어낸 명칭임.
+	public String update(@PathVariable("id") long id, Model model) {
+		System.out.println("@@@@@@@@@@@@@@@@" + id);
+		
+		Optional<Board> opt = boardRepository.findById(id);
+		Board board = opt.get();
+		System.out.println(board);
+		model.addAttribute("board", board);
 
+		
+		return "board/update";
+	}	
+	
+	@PostMapping("/update")
+	@ResponseBody //문자열을 보냄
+	public String boardUpdatePost(@ModelAttribute Board board) {
+		System.out.println(board);
+		// board의 키 (id)가 중복이면 .. 수정기능 수행
+		// board의 키 (id)가 중복이 아니라면 .. 입력 기능 수행
+		// ==================>>>>>>>>>jpa의 방식
+				
+		boardRepository.save(board); //입력 또는 수정
+//		/* 로그인 여부 확인 (세션의 값 확인) */
+//		User user = (User) session.getAttribute("user_info");
+//		if(user == null) {
+//			return "0";
+//		}
+//		else {
+//		String userId = user.getEmail();
+//		board.setUserId(userId);
+//		boardRepository.save(board);
+//		}
+		return "1";
+	}
+	@GetMapping("/delete/{id}")
+	public String delete(
+			@PathVariable ("id") long id) {
+		boardRepository.deleteById(id);
+	return "redirect:/board/";	
+	}
+	
+	
+	
+	
+@GetMapping("/{id}")  // id 라는 명칭은 개발자가 임의로 만들어낸 명칭임.
+public String detail(@PathVariable("id") long id, Model model) {
+	System.out.println("@@@@@@@@@@@@@@@@" + id);
+	
+	Optional<Board> opt = boardRepository.findById(id);
+	Board board = opt.get();
+	System.out.println(board);
+	model.addAttribute("board", board);
+
+	
+	return "board/detail";
+}
+
+
+	
 	@GetMapping("/")
 	public String board(
 			Model model,
@@ -77,6 +137,7 @@ public class BoardController {
 		model.addAttribute("list", list);
 		model.addAttribute("startPage", startPage);
 		model.addAttribute("endPage", endPage);
+		model.addAttribute("totalPage", totalPage);
 		model.addAttribute("page", page);
 		return "board/list";
 	}
